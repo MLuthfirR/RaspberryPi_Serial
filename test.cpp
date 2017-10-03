208d69cb201f35ad6fdcd74d65a93551a7784946
MLuthfirR <muhammad_luthfirrahman@apps.ipb.ac.id>
2017-10-03 14:18:44 +0700

    Tambahan

--- test.cpp
+++ test.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
 #include<iomanip>
+#include<wiringSerial.h>
 using namespace std;
 
 int main(){
 	cout<<"Hello World"<<endl;
+	int handle,available;
+	handle = serialOpen("/dev/ttyAMA0",9600);
+	while(1){
+		available = serialDataAvail(handle);
+		cout<<available<<endl;
+	}
 	return 0;
 }
